36c372ca8c19ef9e296953be2f3ba8822eb1143a
slb19xx <93225634+slb19xx@users.noreply.github.com>
2022-07-18 15:11:11 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Weather from "./Weather.js";
 export default function App() {
   return (
     <div className="App">
-      <h1 className="search">Weather Search</h1>
-      <Weather />
-      <footer>
-        Open Source Code by{" "}
-        <a href="https://www.linkedin.com/in/sharibaldie/" target="_blank">
-          Shari Baldie
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className="container">
+        <h1> Weather Search</h1>
+        <Weather />
+        <footer>
+          Open Source Code by{" "}
+          <a href="https://www.linkedin.com/in/sharibaldie/" target="_blank">
+            Shari Baldie
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+
+export default function Weather() {
+  return <h1>Weather Me This...</h1>;
+}
