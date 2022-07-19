83a391d5522eefcc1f96009810bfc3216adf2b93
slb19xx <93225634+slb19xx@users.noreply.github.com>
2022-07-19 01:32:52 -0400

    Updated icon and temperature sizes

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#ff69b4"
-      size={55}
+      size={50}
       animate={true}
     />
   );
